57a9b8a56758fa11609fd86589bdd0fa2fc524c2
Yazan <yazan.ab.ya94@gmail.com>
2022-02-23 21:30:41 +0100

    Profileinfo activity added.

--- app/src/main/java/com/example/myapplication/OptionsPage.java
+++ app/src/main/java/com/example/myapplication/OptionsPage.java
@@ -5,17 +5,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class OptionsPage extends AppCompatActivity {
     Button Distans;
-    Button semester;
-
+    Button semester,Vabb;
+    ImageButton profileButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options_page);
         Distans = (Button)findViewById(R.id.distansarbete_button);
+        profileButton = findViewById(R.id.mini_profile_icon);
+        String newString;
+        Bundle extras = getIntent().getExtras();
+        newString= extras.getString("username");
         Distans.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -27,6 +32,19 @@ public class OptionsPage extends AppCompatActivity {
             public void onClick(View v) {
                 moveToSemesterAnsokaPage();
             }});
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                moveToProfile(newString);
+            }
+        });
+        Vabb = (Button)findViewById(R.id.vabb_button);
+        Vabb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                moveToVabb();
+            }
+        });
     }
     public void  moveToDistansPage(){
         Intent intent = new Intent(this, DistansPage.class);
@@ -36,4 +54,13 @@ public class OptionsPage extends AppCompatActivity {
         Intent intent = new Intent(this, SemesterAnsokaPage.class);
         startActivity(intent);
     }
+    private void moveToProfile(String uname)
+    {
+        startActivity(new Intent(getApplicationContext(),ProfileInfo.class).putExtra("username",uname));
+    }
+    public void  moveToVabb(){
+        Intent intent = new Intent(this, vabb.class);
+        startActivity(intent);
+    }
 }
+
---
+++ app/src/main/java/com/example/myapplication/ProfileInfo.java
@@ -0,0 +1,145 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ProfileInfo extends AppCompatActivity {
+   private String URL;
+
+   TextView F_name,S_name,email,password,id,Semester_days, flex_tim,new_password,Edit_text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_info);
+        init();
+
+    }
+
+
+    private void init()  {
+        String newString;
+        Bundle extras = getIntent().getExtras();
+        newString= extras.getString("username");
+        Log.d("UserName", "init: "+ newString);
+        Edit_text = findViewById(R.id.EditProfileText);
+        URL="http://192.168.56.1/API/profileInfo.php";
+        F_name = (TextView) findViewById(R.id.firstname_view);
+        S_name = (TextView) findViewById(R.id.SecondName_View);
+        email = (TextView) findViewById(R.id.Email_view);
+        password =(TextView) findViewById(R.id.password_view);
+        id = (TextView) findViewById(R.id.id_view);
+        Semester_days = (TextView) findViewById(R.id.days_view);
+        flex_tim = (TextView) findViewById(R.id.Hours_view);
+        new_password = findViewById(R.id.NewPasswordText);
+
+
+        putdata(newString);
+        //getJsonObject(newString);
+        Edit_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent( getApplicationContext(),EditProfileActivity.class));
+            }
+        });
+        new_password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),NewPasswordActivity.class));
+            }
+        });
+
+    }
+    void putdata(String username)
+    {
+        Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                String [] inputfields = new String[1];
+                inputfields[0]= "username";
+
+                String [] data = new String[2];
+                data[0]=username;
+
+                PutData putData = new PutData(URL,"GET",inputfields,data);
+                if(putData.startPut())
+                {   if(putData.onComplete())
+                    {   String res = putData.getResult();
+                        Log.d("RESUlT",res);
+                        try {
+                            JSONObject jsonObject = new JSONObject(res);
+                            Log.d("JSON", "run: "+jsonObject.getString("firstname"));
+                            F_name.setText(jsonObject.getString("firstname"));
+                            S_name.setText(jsonObject.getString("secondname"));
+                            id.setText(String.valueOf(jsonObject.getInt("id")));
+                            email.setText(jsonObject.getString("email"));
+                            password.setText(jsonObject.getString("password"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }
+
+
+            }
+        });
+    }
+    void getJsonObject(String str)
+    {
+        JsonObjectRequest stringRequest = new JsonObjectRequest(Request.Method.GET, URL,null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+
+                   JSONObject js= response.getJSONObject("username");
+                   String str = js.toString();
+                    Log.d("JSONOBJECT", "onResponse: "+String.valueOf(response.getJSONObject("username"))+ "STR" + str) ;
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),error.toString().trim(),Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> data=new HashMap<>();
+                data.put("username",str);
+
+                return data;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        requestQueue.add(stringRequest);
+    }
+}
